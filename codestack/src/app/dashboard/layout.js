0d2c748e5c6d6b6804a105cc360072941df8742e
innobrightcafe <innobrightcafe@gmail.com>
2024-02-24 06:36:30 -0500

    category bar set

--- codestack/src/app/dashboard/layout.js
+++ codestack/src/app/dashboard/layout.js
@@ -3,8 +3,8 @@ import styles from "@/components/ui/dashboard/dashboard.module.css";
 
 const layout = ({ children }) => {
   return (
-    <div className={styles.container}> 
-        <DashboardNavbar /> 
+    <div className={styles.container}>
+      <DashboardNavbar />
       <div className={styles.content}>{children}</div>
     </div>
   );
--- codestack/src/app/page.jsx
+++ codestack/src/app/page.jsx
@@ -1,11 +1,31 @@
 import MainNavbar from "@/components/ui/navbar/mainNavbar";
- 
+import styles from "@/components/homepage.module.css";
+import { 
+  MdArrowBackIos, 
+  MdArrowForwardIos, 
+} from "react-icons/md";
 
 export default function Home() {
   return (
     <div>
       <MainNavbar />
-      <h1>Home</h1>
+      <div className={styles.container}>
+        <div className={styles.categories}>
+          <MdArrowBackIos size={20} />
+          <div className={styles.cartegory__card}>
+            <div className={styles.category}>
+              <h3>All</h3>
+            </div>
+            <div className={styles.category}>
+              <h3>JavaScript</h3>
+            </div>
+            <div className={styles.category}>
+              <h3>UI/UX</h3>
+            </div>
+          </div>
+          <MdArrowForwardIos size={20} />
+        </div>
+      </div>
     </div>
   );
 }
