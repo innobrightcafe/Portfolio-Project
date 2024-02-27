185ee7dbd7b8277d760d683e3558d425d1ad7786
favydabs <favourdabit@gmail.com>
2024-02-27 12:48:35 +0100

    login page

--- codestack/src/app/(auth)/login/page.jsx
+++ codestack/src/app/(auth)/login/page.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import styles from "./login.module.css";
 
-function LoginPage() {
-  return (
-    <div>LoginPage</div>
+function loginpage () {
+  return(
+<div className={styles.container}>
+<p> Please login with your credentials</p>
+  <div className={styles.formContainer}>
+    <form action="" className={styles.form}>
+      <label>Email</label>
+      <input type="text" placeholder=" Enter Email Address" />
+      <label>password</label>
+      <input type="text" placeholder=" Enter Password" />
+
+      <button>Login</button>
+    </form>
+  </div>
+</div>
+
   )
 }
-
-export default LoginPage
+export default loginpage;
